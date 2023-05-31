f4b1f9b073d11fbdeabec9d6ac2c0f298ee8e5d0
冯伟 <lyman.von9506@gmail.com>
2023-05-31 21:28:50 +0800

    feat: Add stories for svelte node view renderer

---
+++ libs/@tiptap/svelte/stories/NodeView.svelte
@@ -0,0 +1,8 @@
+<script>
+    import { NodeViewContent, NodeViewWrapper } from "..";
+</script>
+
+<NodeViewWrapper>
+    <div class="title">My NodeView</div>
+    <NodeViewContent />
+</NodeViewWrapper>
