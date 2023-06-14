66e10064341dc1e8824337afef007a1c85fdf808
冯伟 <lyman.von9506@gmail.com>
2023-06-14 10:25:16 +0800

    feat: Add more attributes for table celll

--- libs/@tiptap/nodes/lib/tabular/cell.js
+++ libs/@tiptap/nodes/lib/tabular/cell.js
@@ -27,6 +27,62 @@ export const TableCell = Node.create({
                     return Number(dom.getAttribute("colspan") || 1);
                 },
             },
+
+            /**
+             * 垂直水平对齐方式
+             */
+            align: {
+                default: null,
+                parseHTML: (ele) => ele.dataset.align?.split(" ") || ["t", "l"],
+                renderHTML: (attrs) => (attrs.align ? { "data-align": attrs.align.join(" ") } : {}),
+            },
+
+            // numberFormat: {
+            //     default: null,
+            //     parseHTML: (ele) => decompressPMNodeAttr(ele.getAttribute('number_format'), 'General'),
+            //     renderHTML: (attrs) => !attrs.numberFormat ? {} : { number_format: compressPMNodeAttr(attrs.numberFormat) },
+            // },
+
+            backgroundColor: {
+                default: null,
+                parseHTML: (element) => element.getAttribute("data-background-color"),
+                renderHTML: (attrs) => {
+                    if (!attrs.backgroundColor) return {};
+
+                    return {
+                        "data-background-color": attrs.backgroundColor,
+                        style: `background-color: ${attrs.backgroundColor}`,
+                    };
+                },
+            },
+
+            borderColor: {
+                default: null,
+                parseHTML: (ele) => ele.style.borderColor,
+                renderHTML: (attrs) => {
+                    if (!attrs.borderColor) return {};
+                    return {
+                        style: `border-color: ${attrs.borderColor};`,
+                    };
+                },
+            },
+
+            /**
+             * 边框样式
+             * - none 无边框
+             * - solid | null 实线
+             * - dashed 虚线
+             */
+            borderStyle: {
+                default: null,
+                parseHTML: (ele) => ele.style.borderStyle,
+                renderHTML: (attrs) => {
+                    if (!attrs.borderStyle || attrs.borderStyle === "solid") return {};
+                    return {
+                        style: `border-style: ${attrs.borderStyle};`,
+                    };
+                },
+            },
         };
     },
     parseHTML() {
